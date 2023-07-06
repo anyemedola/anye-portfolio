0a39ebb6f77767f73ed2392ea9c62c1429446903
Any Medola <anynhamedola@gmail.com>
2023-07-06 16:28:06 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,4 +23,12 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
-  
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
